349a3598b7c0e62193dc1061d8e62b87f0225c56
Bruce Leong <brucewleong@gmail.com>
2018-02-27 14:41:48 -0500

    product-reducer

--- client/store/products.js
+++ client/store/products.js
@@ -19,25 +19,8 @@ export default function reducer (products = [], action) {
 
 /* ------------       THUNK CREATORS     ------------------ */
 
-export const fetchUsers = () => dispatch => {
-  axios.get('/api/users')
-       .then(res => dispatch(init(res.data)));
+export const fetchProducts = () => dispatch => {
+  axios.get('/api/products')
+       .then(res => dispatch(getProducts(res.data)));
 };
 
-export const removeUser = id => dispatch => {
-  axios.delete(`/api/users/${id}`)
-       .then(() => dispatch(remove(id)))
-       .catch(err => console.error(`Removing user: ${id} unsuccesful`, err));
-};
-
-export const addUser = user => dispatch => {
-  axios.post('/api/users', user)
-       .then(res => dispatch(create(res.data)))
-       .catch(err => console.error(`Creating user: ${user} unsuccesful`, err));
-};
-
-export const updateUser = (id, user) => dispatch => {
-  axios.put(`/api/users/${id}`, user)
-       .then(res => dispatch(update(res.data)))
-       .catch(err => console.error(`Updating user: ${user} unsuccesful`, err));
-};

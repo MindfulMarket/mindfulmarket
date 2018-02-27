d77e28e3262f915bfb5720a73b3a945dc640488a
mfriedland <micahfriedland@gmail.com>
2018-02-27 15:59:51 -0500

    working

--- client/components/Filter.js
+++ client/components/Filter.js
@@ -7,7 +7,18 @@ const Filter = (props) => {
   { /* line
   */ }
       <h4> Brand </h4>
+      <input placeholder="Search" />
       <h4> Price </h4>
+      <div>
+      <input type="checkbox" checked="checked" /> <h5>$0 - $10</h5>
+      </div>
+      <div>
+      <input type="checkbox" checked="checked" /> <h5>$10 - $25</h5>
+      </div>
+      <div>
+      <input type="checkbox" checked="checked" /> <h5>$25 - $100</h5>
+      </div>
+      <h5>$100 and Above</h5>
       <h4> Ratings </h4>
       <h4> Cause </h4>
    </div>

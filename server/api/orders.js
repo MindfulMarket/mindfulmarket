47f864b573af3076445c73fca0988e6fe38505f5
danmcgill6 <danmcgill6@gmail.com>
2018-03-01 11:37:05 -0500

    refactored backend

--- server/api/orders.js
+++ server/api/orders.js
@@ -30,7 +30,6 @@ router.get('/', (req, res, next) => {
     Orders.scope('populated').create(req.body)
     .then(review => res.json(review))
     .catch(next)
-<<<<<<< HEAD
   })
   
   router.put('/:id', (req, res, next) => {
@@ -43,28 +42,6 @@ router.get('/', (req, res, next) => {
         .catch(next)
     })
     
-=======
-})
-
-router.post('/', (req, res, next) => {
-    Orders.create(req.body)
-    .then(order => res.json(order))
-    .catch(next)
-})
-
-router.get('/:id', (req, res, next) => {
-  Orders.findById({include: {all: true}})
-    .then(order => res.json(order))
-    .catch(next)
-})
-
-router.put('/:id', (req, res, next) => {
-  Orders.findById(req, {include: {all: true}})
-    .then(order => res.json(order))
-    .catch(next)
-})
-
->>>>>>> 1fa848dcb8a3535bbd57a2a7fd2ef2ece008dc6d
 
   router.delete('/:id', (req, res, next) => {
     Orders.destroy({ 

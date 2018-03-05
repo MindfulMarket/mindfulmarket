a63c0b5f7107da34712d51f52237398573e2476f
mfriedland <micahfriedland@gmail.com>
2018-03-05 11:35:21 -0500

    updatednavbar and working on search

--- server/api/search.js
+++ server/api/search.js
@@ -3,9 +3,7 @@ const { Products } = require('../db/models')
 module.exports = router
 
 router.post('/', (req, res, next) => {
-    console.log('line 21', req.body)
     let nameParts = req.body.searchCriteria.split(' ')
-    console.log(nameParts)
     let resultArray = [];
     nameParts.forEach(word =>
         Products.scope('populated').findAll({
@@ -14,7 +12,6 @@ router.post('/', (req, res, next) => {
             }
         })
         .then((result) => {
-            console.log(result)
             resultArray.push(result) // return res.json(result)
         })
         .then(() => console.log(resultArray))
